96cb8434a90e82f9f3ab09f7ced4243e525bde0b
anuphongsrinawong <srinawonganuphong@gmail.com>
2023-09-06 09:08:51 +0700

    page index.html  and .json
    1. .json  add "views": "1284",
    2. index.html add id= productlist

--- asset/js/index.js
+++ asset/js/index.js
@@ -8,19 +8,19 @@ fetch('productData.json')
     .then(response => response.json())
     .then(data => {
         productData = data;
-        console.log(productData)
+        // console.log(productData)
         let productDataToUse;
 
         if (productParam > 0 || productParam <= productData.length) {
             productDataToUse = productData[productParam - 1];
-            console.log(productParam)
+            // console.log(productParam)
         } else {
             productDataToUse = productData[0];
             productParam = 1
-            console.log(productParam)
+            // console.log(productParam)
         }
 
-        console.log(productDataToUse)
+        // console.log(productDataToUse)
 
         // ใส่ข้อมูลรูปภาพใน Carousel
         const carouselImagesContainer = document.querySelector(".carousel-inner");
@@ -49,6 +49,35 @@ fetch('productData.json')
 
         const link = document.querySelector("#detail");
         link.href = "detail.html?product=" + productParam;
+
+
+        // หา DOM element ที่มี id="productList"
+        const productList = document.getElementById('productList');
+
+        // สร้างรายการสินค้าและแทรกลงใน HTML
+        data.forEach((product, index) => {
+
+            if (index == productParam - 1) {
+                return; //ข้ามช่องที่ index หรือ ช่องที่เป็นรูปภาพใหญ่ที่เปิด
+            } else if (index >= 5) {
+                return; //ข้ามถ้ามีอื่นๆ ครบ 5 แล้ว
+            };
+
+            const productDiv = document.createElement('div');
+            productDiv.className = 'col-6 col-md-6 col-lg-12 my-3';
+
+            productDiv.innerHTML = `
+                <div class="news-right-pic pic_program">
+                <a href="?product=${index + 1}">
+                    <img style="width: 80%; height: 160px;" src="${product.productDetails.image}" alt="${product.productDetails.productName}">
+                </a>
+                <br>
+                <a href="?product=${index + 1}">${product.productDetails.productName} (ดู : ${product.productDetails.views})</a>
+                </div>
+            `;
+
+            productList.appendChild(productDiv);
+        });
 
     })
     .catch(error => console.error('เกิดข้อผิดพลาดในการดึงข้อมูล:', error));
